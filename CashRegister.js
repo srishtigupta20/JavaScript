b3f78f6def908e42f6d23498d9d0288f1d4ef67d
Srishti Gupta <71086428+srishtigupta20@users.noreply.github.com>
2022-01-02 22:02:44 +0530

    Create CashRegister.js

---
+++ CashRegister.js
@@ -0,0 +1,49 @@
+let currencyUnit = {
+    "PENNY" :1,
+    "NICKEL" :5,
+    "DIME":10,
+    "QUARTER": 25,
+    "ONE":100,
+    'FIVE':500,
+    "TEN":1000,
+    "TWENTY":2000,
+    "ONE HUNDRED":10000
+  }
+function checkCashRegister(price, cash, cid) {
+  
+  let changeSum = cash*100 - price*100;
+  let sumCheck = changeSum;
+  let change = [];
+  let status = '';
+
+  let cidSum = 0;
+  let filterCid = cid.filter(elem => elem[1] !==0).reverse();
+
+  filterCid.forEach(elem => {
+    let curr = elem[0];
+    let currSum = elem[1]*100;
+    cidSum += currSum;
+    let amount = 0;
+    while(changeSum >= currencyUnit[curr] && currSum >0){
+      amount += currencyUnit[curr];
+      changeSum -= currencyUnit[curr];
+      currSum -= currencyUnit[curr]; 
+    }
+    if(amount !== 0){
+      change.push([curr,amount/100]);
+    }
+  });
+  if(changeSum>0){
+    status = 'INSUFFICIENT_FUNDS';
+    change = [];
+  }else if(changeSum == 0 && sumCheck == cidSum){
+    status = 'CLOSED';
+    change = cid;
+  }else{
+    status = 'OPEN';
+  }
+  return {'status':status,'change': change};
+}
+
+let output = checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
+console.log(output);
